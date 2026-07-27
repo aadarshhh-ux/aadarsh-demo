52ab60de68b93aeaa694d2e102f0fa5d98398d13
aadarsh <aadarshhhdubeyyy@gmail.com>
2026-07-27 16:59:33 +0530

    new update

--- aadarsh.py
+++ aadarsh.py
@@ -1,9 +1,35 @@
-print("hello")
-print("My name is Aadarsh")
-name = "Aadarsh"
-age = 20
-price = 100.66
-print("my name is ",name,"and my age is ",age,)
-print(type(name))
-print(type(age))
-print(type(price))
+"""#print("hello")
+#print("My name is Aadarsh")
+#name = "Aadarsh"
+#age = 20
+#price = 100.66
+#old = True
+#ad = None
+#print("my name is ",name,"and my age is ",age,)
+#print(type(name))
+#print(type(age))
+#print(type(price))
+#print(type(old))
+#print(type(ad))"""
+
+#airthmatic oprations
+"""a = 2
+b = 3
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b)#remainder
+print(a**b)#power #a ki power b"""
+
+#relational oprations
+a = 33
+b = 55
+print(a==b) #answer is false
+print(a!=b) #answer is true
+print(a>b) #answer is false
+print(a<b) #answer is true
+print(a>=b) #answer is false
+print(a<=b) #answer is true 
+
+
