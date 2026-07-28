f40979df799baac10cd2f4f751b56941927e6192
aadarsh <aadarshhhdubeyyy@gmail.com>
2026-07-28 22:40:06 +0530

    new code with if else

--- aadarsh.py
+++ aadarsh.py
@@ -119,14 +119,78 @@ print("length of final string is:", length)"""
 """str = "aadarsh dubey"
 print(str[1])  # Output: a
 str1 = " my age is 20 "
-print(str1[3]) """ # Output: # it also print empty space because space is also a character
+print(str1[3])  # Output: # it also print empty space because space is also a character"""
 
 #slicing in string
 #for example if we want to find 1st character of string we can use index
 #also we have negative indexing in string
-str = "aadarsh dubey"
+"""str = "aadarsh dubey"
 print(str[0:3])  # Output: aad
 print(str[8:len(str)]) # Output:  dubey # len(str) is used to find the length of string it will print till the end of string
 print(str[:4]) #[0:4]
 print(str[5:]) #[5:len(str)]
-print(str[-5:-1]) # Output: ube # it will print from 5th last character to 1st last character
+print(str[-5:-1]) # Output: ube # it will print from 5th last character to 1st last character"""
+
+#string functions
+"""str = "hii my name is aadarsh"
+str1 = "my age is 30"
+print(str.endswith("rsh")) # Output: True
+print(str.startswith("hii")) # Output: True
+print(str.capitalize()) # Output: Hii my name is aadarsh
+print(str1.replace("30","20"))# Output: my age is 20
+print(str1.find("age")) # Output: 3 # it will return the index of the first occurrence of the substring
+print(str1.count("is")) # Output: 2 # it will return the number of occurrences of the substring"""
+
+
+#WAP to input users name and print the length of the name
+"""name = input("enter name")
+print("length of name is ",len(name))"""
+
+# if else statement
+"""light = input("Enter the color of the traffic light (red, yellow, green): ")
+
+if (light == "red"):
+    print("Stop")
+elif (light == "yellow"):
+    print("Get ready to move")
+elif (light == "green"):
+    print("Go")
+else:
+    print("Invalid color")
+
+age = int(input("Enter your age: "))
+if(age >= 18):
+    if(age>=90):
+        print("you are too old to drive")
+    else:
+        print("you can drive")
+else:
+    print("you are too young to drive")"""
+
+
+#WAP to check is a number entered by user is even or odd
+"""int = int(input("Enter a number: "))
+if (int%2 ==0):
+    print("number is even")
+else:
+    print("number is odd")"""
+
+#WAP to finde the greatest of 3 number entered by the user 
+"""a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+if (a >= b) and (a >= c):
+    print("The greatest number is:", a)
+elif (b >= a) and (b >= c):
+    print("The greatest number is:", b)
+else:
+    print("The greatest number is:", c)"""
+
+#WAP to check if a number is a multiple of 7 or not
+num = int(input("Enter a number: "))
+if (num % 7 == 0):
+    print(num, "is a multiple of 7")
+else:
+    print(num, "is not a multiple of 7")
+    
+
