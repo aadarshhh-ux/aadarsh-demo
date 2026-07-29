635c20e4d7d4a3612bddb3453d4035160880a074
aadarsh <aadarshhhdubeyyy@gmail.com>
2026-07-29 17:28:06 +0530

    new

--- aadarsh.py
+++ aadarsh.py
@@ -252,8 +252,12 @@ else:
     print("not")"""
 
 #WAP to count the number of students with "A" grade in the following tuple ["C","D","A","A","B","B","A"]
-grade = ["C","D","A","A","B","B","A"]
-print(grade.count("A"))
+"""grade = ["C","D","A","A","B","B","A"]
+print(grade.count("A"))"""
 
+# store the above values in a list & sort them from "A" to "D"
+list = ["C","D","A","A","B","B","A"]
+list.sort()
+print(list)
 
 
