a777e50bfc68b96c1faa2647ea73194ca15da28e
aadarsh <aadarshhhdubeyyy@gmail.com>
2026-07-31 19:15:44 +0530

    new

--- aadarsh.py
+++ aadarsh.py
@@ -348,12 +348,17 @@ while i>=1: #stoping condition
     i-=1"""
 
 #print the multiplication table of a number n=3
-i = 1
+"""i = 1
 while i<=10:
     print(3*i)
-    i+=1
+    i+=1"""
 
-
+#print the elements of the following list using a loop [1,4,9,16,25,36,49,64,81,100]
+num = [1,4,9,16,25,36,49,64,81,100]
+idx = 0
+while idx < len(num):
+    print(num[idx])
+    idx +=1
 
 
 
