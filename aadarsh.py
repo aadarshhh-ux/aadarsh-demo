eb774731b3db089708220a802f5c58ffa07bcdfe
aadarsh <aadarshhhdubeyyy@gmail.com>
2026-08-12 16:31:16 +0530

    new

--- aadarsh.py
+++ aadarsh.py
@@ -448,12 +448,20 @@ for i in range(1,11):
     print(n*i)"""
 
 #pass statement
-for i in range (10):
+"""for i in range (10):
     pass #it will do nothing it is used as a placeholder
 
-print("aadarsh")
+print("aadarsh")"""
+
+#wap to find the sum for natural number
+n = 5 
+
+sum = 0 
+for i in range(1,n+1):
+    sum += i
 
 
+print ("total sum", sum)
 
 
 
