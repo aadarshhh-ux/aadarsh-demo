b45be0001bc77fc15db6cc0a7c8d55a317c89d2e
aadarsh <aadarshhhdubeyyy@gmail.com>
2026-08-12 16:04:55 +0530

    forloop

--- aadarsh.py
+++ aadarsh.py
@@ -413,11 +413,20 @@ else:
     print("loop end") """   
 
 # print the elements of the following list using a for loop [1,4,9,16,25,36,49,64,81,100]
-num = [1,4,9,16,25,36,49,64,81,100]
+"""num = [1,4,9,16,25,36,49,64,81,100]
 for val in  num :
-    print(val)
+    print(val)"""
 
-
+#search for a number x in this tuple using loop (1,4,9,16,25,36,49,64,81,100)
+num = (1,4,9,16,25,36,49,64,81,100)
+x = int(input("enter number"))
+for i in num:
+    if i == x:
+        print("found")
+        break
+    else:
+        print("not found")
+        
 
 
 
